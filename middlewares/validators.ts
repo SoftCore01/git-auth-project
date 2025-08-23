d6af66a672213f408adb30729cb2fad3f002a84e
SoftCore <soft0core1@gmail.com>
2025-08-23 20:10:47 +0100

    1.login schema

--- middlewares/validators.ts
+++ middlewares/validators.ts
@@ -1,7 +1,5 @@
 import Joi from "joi";
 
-
-
 export const signUpSchema = Joi.object({
   firstname: Joi.string().min(6).max(50).required(),
   lastname: Joi.string().min(6).max(50).required(),
@@ -13,3 +11,11 @@ export const signUpSchema = Joi.object({
     .required(),
 });
 
+export const loginSchema = Joi.object({
+  email: Joi.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/),
+  password: Joi.string()
+    .min(6)
+    .max(50)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/)
+    .required(),
+});
